295f81b05d182582bfb295da97b9637fb456ec79
Ibrahim Chowdhury <ihchowdhury32@gmail.com>
2023-08-29 15:31:33 +0600

    first commit

---
+++ src/main/java/com/example/practicecicd/PracticeCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicecicd/PracticeCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
